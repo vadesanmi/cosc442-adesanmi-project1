49ddfce5c8369ade7fe7231b850cd6fc10c8ae96
Victor Adesanmi <vadesa1@students.towson.edu>
2016-02-18 13:39:54 -0500

    added javadoc to monopoly program.

--- Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/CardCell.java
+++ Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/CardCell.java
@@ -1,15 +1,34 @@
 package edu.towson.cis.cosc442.project1.monopoly;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class CardCell.
+ */
 public class CardCell extends Cell {
+    
+    /**
+     * Instantiates a new card cell.
+     *
+     * @param type the type
+     * @param name the name
+     */
     public CardCell(int type, String name) {
         setName(name);
         this.type = type;
     }
     
+    /* (non-Javadoc)
+     * @see edu.towson.cis.cosc442.project1.monopoly.Cell#playAction(java.lang.String)
+     */
     public Boolean playAction(String msg) {
     	return true;
     }
     
+    /**
+     * Gets the type.
+     *
+     * @return the type
+     */
     public int getType() {
         return type;
     }
--- Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/GameBoard.java
+++ Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/GameBoard.java
@@ -3,18 +3,38 @@ package edu.towson.cis.cosc442.project1.monopoly;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class GameBoard.
+ */
 public class GameBoard {
 
+	/** The cells. */
 	private ArrayList<Cell> cells = new ArrayList<Cell>();
+    
+    /** The chance cards. */
     private ArrayList<Card> chanceCards = new ArrayList<Card>();
+	
+	/** The color groups. */
 	//the key of colorGroups is the name of the color group.
 	private Hashtable<String, Integer> colorGroups = new Hashtable<String, Integer>();
+	
+	/** The community chest cards. */
 	private ArrayList<Card> communityChestCards = new ArrayList<Card>();
+	
+	/**
+	 * Instantiates a new game board.
+	 */
 	public GameBoard() {
 		Cell go = new GoCell();
 		addCell(go);
 	}
 
+    /**
+     * Adds the card.
+     *
+     * @param card the card
+     */
     public void addCard(Card card) {
         if(card.getCardType() == Card.TYPE_CC) {
             communityChestCards.add(card);
@@ -23,16 +43,31 @@ public class GameBoard {
         }
     }
 	
+	/**
+	 * Adds the cell.
+	 *
+	 * @param cell the cell
+	 */
 	public void addCell(Cell cell) {
 		cells.add(cell);
 	}
 	
+	/**
+	 * Adds the cell.
+	 *
+	 * @param cell the cell
+	 */
 	public void addCell(PropertyCell cell) {
 		int propertyNumber = getPropertyNumberForColor(cell.getColorGroup());
 		colorGroups.put(cell.getColorGroup(), new Integer(propertyNumber + 1));
         cells.add(cell);
 	}
 
+    /**
+     * Draw cc card.
+     *
+     * @return the card
+     */
     public Card drawCCCard() {
         Card card = (Card)communityChestCards.get(0);
         communityChestCards.remove(0);
@@ -40,6 +75,11 @@ public class GameBoard {
         return card;
     }
 
+    /**
+     * Draw chance card.
+     *
+     * @return the card
+     */
     public Card drawChanceCard() {
         Card card = (Card)chanceCards.get(0);
         chanceCards.remove(0);
@@ -47,14 +87,31 @@ public class GameBoard {
         return card;
     }
 
+	/**
+	 * Gets the cell.
+	 *
+	 * @param newIndex the new index
+	 * @return the cell
+	 */
 	public Cell getCell(int newIndex) {
 		return (Cell)cells.get(newIndex);
 	}
 	
+	/**
+	 * Gets the cell number.
+	 *
+	 * @return the cell number
+	 */
 	public int getCellNumber() {
 		return cells.size();
 	}
 	
+	/**
+	 * Gets the properties in monopoly.
+	 *
+	 * @param color the color
+	 * @return the properties in monopoly
+	 */
 	public PropertyCell[] getPropertiesInMonopoly(String color) {
 		PropertyCell[] monopolyCells = 
 			new PropertyCell[getPropertyNumberForColor(color)];
@@ -72,6 +129,12 @@ public class GameBoard {
 		return monopolyCells;
 	}
 	
+	/**
+	 * Gets the property number for color.
+	 *
+	 * @param name the name
+	 * @return the property number for color
+	 */
 	public int getPropertyNumberForColor(String name) {
 		Integer number = (Integer)colorGroups.get(name);
 		if(number != null) {
@@ -80,6 +143,12 @@ public class GameBoard {
 		return 0;
 	}
 
+	/**
+	 * Query cell.
+	 *
+	 * @param string the string
+	 * @return the cell
+	 */
 	public Cell queryCell(String string) {
 		for(int i = 0; i < cells.size(); i++){
 			Cell temp = (Cell)cells.get(i); 
@@ -90,6 +159,12 @@ public class GameBoard {
 		return null;
 	}
 	
+	/**
+	 * Query cell index.
+	 *
+	 * @param string the string
+	 * @return the int
+	 */
 	public int queryCellIndex(String string){
 		for(int i = 0; i < cells.size(); i++){
 			Cell temp = (Cell)cells.get(i); 
@@ -100,6 +175,9 @@ public class GameBoard {
 		return -1;
 	}
 
+    /**
+     * Removes the cards.
+     */
     public void removeCards() {
         communityChestCards.clear();
     }
